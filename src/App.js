1f5ffd5e0036d24a672c36e320236fa5360633e9
Terran Blake <terranblake@gmail.com>
2020-05-04 19:07:06 -0700

    customize message

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <code>Hello world!</code>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <code>Hello world!</code>
+          Learn React
         </a>
       </header>
     </div>
