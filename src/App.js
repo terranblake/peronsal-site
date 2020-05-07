adc7bdeb25cb36327443926a1c82ed08b1c8ca89
Terran Blake <terranblake@gmail.com>
2020-05-06 20:11:14 -0700

    setup basic configuration for apollo server

--- src/App.js
+++ src/App.js
@@ -2,23 +2,55 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+import { ApolloProvider, Query } from 'react-apollo';
+
+const apolloServerURI = process.env.APOLLO_SERVER_URI;
+
+const client = apolloServerURI
+  ? new ApolloClient({
+    uri: process.env.APOLLO_SERVER_URI
+  })
+  : null;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <code>Hello world!</code>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  return client
+    ? (
+      <ApolloProvider client={client}>
+        <div>Hello World</div>
+        <Query
+          query={
+            gql`
+              {
+                posts {
+                  title
+                  createdAt
+                }
+              }
+            `
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {
+            (data) => {
+              data = data.data
+              if (!data || data.posts === undefined) return null;
+
+              return (
+                <ul>
+                  {
+                    data.posts.map(({ title, createdAt }) => {
+                      return <li key={createdAt}>{createdAt} - {title}</li>
+                    })
+                  }
+                </ul>
+              )
+            }
+          }
+        </Query>
+      </ApolloProvider>
+    )
+    : <div>apollo server not configured</div>
 }
 
 export default App;
